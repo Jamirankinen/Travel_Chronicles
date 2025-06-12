12f2dba37b799de669217c3e887a285706b0fab3
Jamiiiiii <jami34@windowslive.com>
2025-06-12 16:09:54 +0300

    blobs off from jsx

--- travel_chronicles/src/components/Animations/Background.tsx
+++ travel_chronicles/src/components/Animations/Background.tsx
@@ -4,9 +4,6 @@ import styles from './Background.module.css';
 export default function Background() {
   return (
     <div className={styles.background}>
-      <div className={`${styles.blob} ${styles.blob1}`} />
-      <div className={`${styles.blob} ${styles.blob2}`} />
-      <div className={`${styles.blob} ${styles.blob3}`} />
     </div>
   );
 }
